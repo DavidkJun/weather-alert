b8fbf28e041f3e8bd3fa52686dc71ac62ab66a84
Davyd Krykota <krikotadavid957@gmail.com>
2025-05-26 22:47:42 +0300

    feat: add weather api call

---
+++ src/weather/weather.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import axios from 'axios';
+import { ConfigService } from '../config/config.service';
+
+@Injectable()
+export class WeatherService {
+  constructor(private readonly configService: ConfigService) {}
+
+  async getForecast(city: string = 'Kyiv') {
+    const apiKey = this.configService.get('OPENWEATHER_API_KEY');
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const { data } = await axios.get(url);
+    return data;
+  }
+}
