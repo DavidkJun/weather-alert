862b313cf040a80d62b9682684d80b4d30bdc341
Lev <lev.bereza@gmail.com>
2025-06-03 12:17:27 +0300

    feat: add controller to get weather for specified city

--- src/weather/weather.module.ts
+++ src/weather/weather.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { WeatherService } from './weather.service';
 import {ConfigSetupModule} from "../config/config.module";
+import {WeatherController} from "./weather.controller";
 
 @Module({
   imports: [ConfigSetupModule],
+  controllers: [WeatherController],
   providers: [WeatherService],
   exports: [WeatherService],
 })
